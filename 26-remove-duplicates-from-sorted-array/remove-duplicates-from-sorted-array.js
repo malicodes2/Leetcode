8d014715aa1cf11ee85a8c37d4143f4ca617b579
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2023-12-04 20:20:04 +0500

    Time: 58 ms (80.65%) | Memory: 44.4 MB (67.55%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let left = 1;
+
+    for (let right = 1; right < nums.length; right++) {
+        if (nums[right] != nums[right - 1]) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+    return left;
+};
