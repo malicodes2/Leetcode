086574b9b74a6da7518e509d37ce4cb88e0b1e3d
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2023-12-21 18:56:35 +0500

    Time: 54 ms (49.01%) | Memory: 42.5 MB (75.69%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function (nums) {
+    let negCount = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0) {
+            return 0;
+        }
+        else if (nums[i] < 0) {
+            negCount++;
+        }
+    }
+    return negCount % 2 == 0 ? 1 : -1;
+};
