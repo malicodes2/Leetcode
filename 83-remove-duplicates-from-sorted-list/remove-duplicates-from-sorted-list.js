66761ee1e9f1a7c008bbd79db572cc1e2e046df5
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-01-08 18:37:07 +0500

    Time: 64 ms (43.96%) | Memory: 44.2 MB (40.03%) - LeetSync

---
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let curr = head;
+    while (curr) {
+        while (curr.next && curr.next.val == curr.val) {
+            curr.next = curr.next.next;
+        }
+        curr = curr.next;
+    }
+    return head;
+};
