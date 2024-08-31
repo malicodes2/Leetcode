478cfdd6b0fdf03dff7bcbd54cd1082a7486e9bb
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-08-31 19:45:56 +0500

    Time: 84 ms (20.09%) | Memory: 53.8 MB (37.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,15 +4,15 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length != t.length) {
+    /*if (s.length != t.length) {
         return false;
-    }
+    }*/
     let sSorted = s.split("").sort().join("");
     let tSorted = t.split("").sort().join("");
-    for (let i = 0; i < tSorted.length; i++) {
+    /*for (let i = 0; i < tSorted.length; i++) {
         if (sSorted[i] != tSorted[i]) {
             return false;
         }
-    }
-    return true;
+    }*/
+    return (sSorted == tSorted);
 };
