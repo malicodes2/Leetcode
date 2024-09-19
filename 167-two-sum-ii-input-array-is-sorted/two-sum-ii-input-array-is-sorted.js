e503a5e0eba4c75d6a5ffb0a2430e268576bbe40
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-09-19 17:26:18 +0500

    Time: 48 ms (91.62%) | Memory: 49.9 MB (69.87%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -7,7 +7,7 @@ var twoSum = function (numbers, target) {
     let l = 0;
     let r = numbers.length - 1;
 
-    while (l < r) {
+    while (l <= r) {
         let sum = numbers[l] + numbers[r];
         if (sum == target) {
             return [l + 1, r + 1];
