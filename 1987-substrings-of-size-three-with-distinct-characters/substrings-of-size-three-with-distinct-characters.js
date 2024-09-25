70d0ae05ee6a5a6a4182f9962d410f0e89eca2b1
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-09-25 18:21:02 +0500

    Time: 57 ms (51.21%) | Memory: 51.1 MB (38.85%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.js
@@ -3,9 +3,15 @@
  * @return {number}
  */
 var countGoodSubstrings = function (s) {
+    if (s.length < 3) {
+        return 0;
+    }
+
     let count = 0;
-    for (let i = 1; i < s.length - 1; i++) {
-        if (s[i] != s[i - 1] && s[i] != s[i + 1] && s[i - 1] != s[i + 1]) {
+    for (let i = 0; i <= s.length - 3; i++) {
+        let substr = s.substring(i, i + 3);
+        let uniqueChars = new Set(substr);
+        if (uniqueChars.size === 3) {
             count++;
         }
     }
