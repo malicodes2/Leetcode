db8ea3987e1b9db60e169ac3015c3fa25f3c43b8
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-09-27 17:24:17 +0500

    Time: 46 ms (92.34%) | Memory: 48.9 MB (89.54%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.js
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -4,39 +4,61 @@
  * @return {boolean}
  */
 var backspaceCompare = function (s, t) {
-    let strS = [];
-    let countS = 0;
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] == '#') {
-            countS++;
-            continue;
-        }
-        else {
-            if (countS > 0) {
-                countS--;
+    compareStrings = (str) => {
+        let arr = [];
+        let count = 0;
+        for (let i = str.length - 1; i >= 0; i--) {
+            if (str[i] == '#') {
+                count++;
+                continue;
             }
             else {
-                strS.push(s[i]);
+                if (count > 0) {
+                    count--;
+                }
+                else {
+                    arr.push(str[i]);
+                }
             }
         }
+        return arr.join('');
     }
+    // let strS = [];
+    // let countS = 0;
+    // for (let i = s.length - 1; i >= 0; i--) {
+    //     if (s[i] == '#') {
+    //         countS++;
+    //         continue;
+    //     }
+    //     else {
+    //         if (countS > 0) {
+    //             countS--;
+    //         }
+    //         else {
+    //             strS.push(s[i]);
+    //         }
+    //     }
+    // }
 
-    let strT = [];
-    let countT = 0;
-    for (let i = t.length - 1; i >= 0; i--) {
-        if (t[i] == '#') {
-            countT++;
-            continue;
-        }
-        else {
-            if (countT > 0) {
-                countT--;
-            }
-            else {
-                strT.push(t[i]);
-            }
-        }
-    }
+    // let strT = [];
+    // let countT = 0;
+    // for (let i = t.length - 1; i >= 0; i--) {
+    //     if (t[i] == '#') {
+    //         countT++;
+    //         continue;
+    //     }
+    //     else {
+    //         if (countT > 0) {
+    //             countT--;
+    //         }
+    //         else {
+    //             strT.push(t[i]);
+    //         }
+    //     }
+    // }
 
-    return strS.join('') == strT.join('');
+    strS = compareStrings(s);
+    strT = compareStrings(t);
+
+    return strS == strT;
 };
