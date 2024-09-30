9df2279f11b673f8448cfe987b99d77d4252011e
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-09-30 17:31:47 +0500

    Time: 64 ms (19.26%) | Memory: 50 MB (51.84%) - LeetSync

---
+++ 682-baseball-game/baseball-game.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+    let stack = [];
+
+    for (let ops of operations) {
+        if (ops == '+') {
+            stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+        }
+        else if (ops == 'D') {
+            stack.push(2 * stack[stack.length - 1]);
+        }
+        else if (ops == 'C') {
+            stack.pop();
+        }
+        else {
+            stack.push(parseInt(ops))
+        }
+    }
+    return stack.reduce((a, b) => a + b, 0);
+};
