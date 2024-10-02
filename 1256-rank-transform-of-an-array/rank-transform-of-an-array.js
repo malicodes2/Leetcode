f312e6770e6763e0963fd8dbcf220673c7a80b0c
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-10-02 07:32:59 +0500

    Time: 153 ms (70.28%) | Memory: 66.7 MB (95.28%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function (arr) {
+    if (arr.length == 0) {
+        return [];
+    }
+
+    let sortedArr = [...arr].sort((a, b) => a - b);
+
+    let map = new Map();
+    let rank = 1;
+
+    for (let num of sortedArr) {
+        if (!map.has(num)) {
+            map.set(num, rank++);
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = map.get(arr[i]);
+    }
+
+    return arr;
+};
