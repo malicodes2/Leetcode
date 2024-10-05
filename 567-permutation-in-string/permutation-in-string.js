3bd06e8c3de50e6bb6e6f02e9ab1cde6ea5092ac
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-10-06 00:09:25 +0500

    Time: 58 ms (95.73%) | Memory: 51.4 MB (85.36%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+    if (s1.length > s2.length) {
+        return false;
+    }
+    let s1Count = Array(26).fill(0);
+    let s2Count = Array(26).fill(0);
+    for (let i = 0; i < s1.length; i++) {
+        s1Count[s1.charCodeAt(i) - 97] += 1
+        s2Count[s2.charCodeAt(i) - 97] += 1
+    }
+
+    for (let i = 0; i < s2.length - s1.length; i++) {
+        if (matches(s1Count, s2Count)) return true;
+        s2Count[s2.charCodeAt(i) - 97] -= 1;
+        s2Count[s2.charCodeAt(i + s1.length) - 97] += 1;
+    }
+
+    return matches(s1Count, s2Count);
+};
+const matches = (s1Count, s2Count) => {
+    for (let i = 0; i < 26; i++) {
+        if (s1Count[i] !== s2Count[i]) return false;
+    }
+    return true;
+}
