cd7bbfc12d0d32286b05afa35853bbaeffe5b699
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-10-10 18:12:20 +0500

    Time: 86 ms (35.98%) | Memory: 66.7 MB (7.47%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,10 +2,21 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    nums.sort();
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] == nums[i - 1]) {
+var containsDuplicate = function (nums) {
+    // nums.sort();
+    // for (let i = 1; i < nums.length; i++) {
+    //     if (nums[i] == nums[i - 1]) {
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+    let hash = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash.has(nums[i])) {
+            hash.set(nums[i], 0);
+        }
+        else {
             return true;
         }
     }
