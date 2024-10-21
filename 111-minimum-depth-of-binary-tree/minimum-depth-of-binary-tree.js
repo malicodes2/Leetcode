4b5f616c774f4fd9d9957e13a32533e7a767b2ee
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-10-21 18:47:04 +0500

    Time: 6 ms (98.53%) | Memory: 83.8 MB (84.59%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.js
@@ -10,18 +10,27 @@
  * @param {TreeNode} root
  * @return {number}
  */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var minDepth = function (root) {
-    if (root == null) return 0;
+    // Base case: If the tree is empty
+    if (!root) return 0;
 
-    if (root.left == null && root.right == null) return 1;
+    // If the left subtree is null, recur on the right subtree
+    if (!root.left) return minDepth(root.right) + 1;
+    // If the right subtree is null, recur on the left subtree
+    if (!root.right) return minDepth(root.left) + 1;
 
-    if (root.left == null) {
-        return 1 + minDepth(root.right);
-    }
-
-    if (root.right == null) {
-        return 1 + minDepth(root.left);
-    }
-
+    // If both left and right are present, find the minimum depth of both subtrees
     return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
