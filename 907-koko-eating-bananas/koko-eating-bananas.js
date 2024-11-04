9f1adaa9d31444a5af2e877f5f2846380f05d7f7
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-11-04 09:56:55 +0500

    Time: 10 ms (63.15%) | Memory: 52.4 MB (66.67%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.js
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -8,18 +8,18 @@ var minEatingSpeed = function (piles, h) {
     let res = r;
 
     while (l <= r) {
-        let k = Math.floor((l + r) / 2);
+        let mid = Math.floor((l + r) / 2);
         let hours = 0;
 
         for (let p of piles) {
-            hours += Math.ceil(p / k);
+            hours += Math.ceil(p / mid);
         }
 
         if (hours <= h) {
-            res = Math.min(res, k);
-            r = k - 1;
+            res = Math.min(res, mid);
+            r = mid - 1;
         } else {
-            l = k + 1;
+            l = mid + 1;
         }
     }
     return res;
