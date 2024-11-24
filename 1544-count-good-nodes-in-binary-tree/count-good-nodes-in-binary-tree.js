c1a1b04c558b877a8a4237243bf4e4c2115d3f76
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-11-24 06:11:42 +0500

    Time: 133 ms (37.22%) | Memory: 69.5 MB (78.65%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
@@ -10,17 +10,37 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var goodNodes = function (root) {
-    const dfs = (node, maxVal) => {
-        if (!node) {
-            return 0;
-        }
+// var goodNodes = function (root) {
+//     const dfs = (node, maxVal) => {
+//         if (!node) {
+//             return 0;
+//         }
 
-        let res = node.val >= maxVal ? 1 : 0;
-        maxVal = Math.max(maxVal, node.val);
-        res += dfs(node.left, maxVal);
-        res += dfs(node.right, maxVal);
-        return res;
+//         let res = node.val >= maxVal ? 1 : 0;
+//         maxVal = Math.max(maxVal, node.val);
+//         res += dfs(node.left, maxVal);
+//         res += dfs(node.right, maxVal);
+//         return res;
+//     }
+//     return dfs(root, root.val);
+// };
+
+const goodNodes = (root) => {
+    let res = 0;
+    let q = new Queue();
+    q.push([root, -Infinity]);
+
+    while(!q.isEmpty()) {
+        let [node, maxVal] = q.pop();
+        if(node.val >= maxVal) {
+            res++;
+        }
+        if(node.left){
+            q.push([node.left, Math.max(maxVal, node.val)])
+        }
+        if(node.right){
+            q.push([node.right, Math.max(maxVal, node.val)])
+        }
     }
-    return dfs(root, root.val);
+    return res;
 };
