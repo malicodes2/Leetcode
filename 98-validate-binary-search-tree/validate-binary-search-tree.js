3c3fb0a78254345bfd9c8ba1c8a35fcff37c149c
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-11-24 06:57:40 +0500

    Time: 0 ms (100.00%) | Memory: 52.6 MB (70.91%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+    //DFS
+    const valid = (node, left, right) => {
+        if (node == null) {
+            return true;
+        }
+        if (!(node.val < right && node.val > left)) {
+            return false;
+        }
+
+        return valid(node.left, left, node.val) &&
+            valid(node.right, node.val, right)
+    }
+    return valid(root, -Infinity, Infinity);
+};
