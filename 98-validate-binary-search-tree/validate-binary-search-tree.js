0be31e5ca4cccc34160eb8f5eeccc31ce0c3c0e3
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-11-24 07:51:16 +0500

    Time: 10 ms (5.76%) | Memory: 57.3 MB (5.14%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -10,18 +10,43 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+// var isValidBST = function (root) {
+//     //DFS
+//     const valid = (node, left, right) => {
+//         if (node == null) {
+//             return true;
+//         }
+//         if (!(node.val < right && node.val > left)) {
+//             return false;
+//         }
+
+//         return valid(node.left, left, node.val) && valid(node.right, node.val, right)
+//     }
+//     return valid(root, -Infinity, Infinity);
+// };
+
+
+
 var isValidBST = function (root) {
-    //DFS
-    const valid = (node, left, right) => {
-        if (node == null) {
-            return true;
-        }
-        if (!(node.val < right && node.val > left)) {
+    if (root === null) {
+        return true;
+    }
+
+    const queue = new Queue([[root, -Infinity, Infinity]]);
+
+    while (!queue.isEmpty()) {
+        const [node, left, right] = queue.pop();
+
+        if (!(left < node.val && node.val < right)) {
             return false;
         }
-
-        return valid(node.left, left, node.val) &&
-            valid(node.right, node.val, right)
+        if (node.left) {
+            queue.push([node.left, left, node.val]);
+        }
+        if (node.right) {
+            queue.push([node.right, node.val, right]);
+        }
     }
-    return valid(root, -Infinity, Infinity);
-};
+
+    return true;
+}
