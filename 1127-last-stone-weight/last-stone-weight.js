a77863d933a27beebb60bdb9eb27f6983fa1c50c
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2024-12-02 12:47:16 +0500

    Time: 13 ms (10.07%) | Memory: 52.2 MB (17.21%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -2,14 +2,21 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function(stones) {
-    while (stones.length > 1) {
-        stones.sort((a, b) => a - b);
+var lastStoneWeight = function (stones) {
+    const maxHeap = new MaxPriorityQueue({ priority: (weight) => weight });
 
-        let x = stones.pop();
-        let y = stones.pop();
-
-        stones.push(Math.abs(x - y))
+    for (const stone of stones) {
+        maxHeap.enqueue(stone);
     }
-    return stones.length > 0 ? stones[0] : 0;
+
+    while (maxHeap.size() > 1) {
+        const stoneOne = maxHeap.dequeue().element;
+        const stoneTwo = maxHeap.dequeue().element;
+
+        if (stoneOne !== stoneTwo) {
+            maxHeap.enqueue(stoneOne - stoneTwo);
+        }
+    }
+
+    return maxHeap.size() === 1 ? maxHeap.dequeue().element : 0;
 };
