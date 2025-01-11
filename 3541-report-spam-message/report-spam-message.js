e153ed4cba4e99b3df2e5db8d950bf5dfb4ecebd
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-01-11 20:54:09 +0500

    Time: 63 ms (56.96%) | Memory: 115.3 MB (74.68%) - LeetSync

--- 3541-report-spam-message/report-spam-message.js
+++ 3541-report-spam-message/report-spam-message.js
@@ -4,20 +4,18 @@
  * @return {boolean}
  */
 var reportSpam = function (message, bannedWords) {
-    let hash = new Map();
+    let hash = new Set(bannedWords);
     let count = 0;
-
-    for (let word of bannedWords) {
-        hash.set(word, 1);
-    }
 
     for (let i = 0; i < message.length; i++) {
         if (hash.has(message[i])) {
             count++;
         }
-
-        if (count >= 2) return true;
     }
 
-    return false;
+    if (count >= 2) {
+        return true;
+    } else {
+        return false;
+    }
 };
