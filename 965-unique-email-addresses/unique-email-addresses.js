41bcee4cccf8f84d81486b6961715c7e9d1aa965
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-01-15 19:18:17 +0500

    Time: 11 ms (66.60%) | Memory: 53.8 MB (60.39%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.js
+++ 965-unique-email-addresses/unique-email-addresses.js
@@ -2,13 +2,24 @@
  * @param {string[]} emails
  * @return {number}
  */
-var numUniqueEmails = function(emails) {
+var numUniqueEmails = function (emails) {
     const unique = new Set();
 
     for (let e of emails) {
-        let [local, domain] = e.split('@');
-        local = local.split('+')[0];
-        local = local.replace(/\./g, '');
+        let i = 0;
+        let local = '';
+
+        while (i < e.length && e[i] !== '@' && e[i] !== '+') {
+            if (e[i] !== '.') {
+                local += e[i];
+            }
+            i++;
+        }
+
+        while (i < e.length && e[i] !== '@') {
+            i++;
+        }
+        const domain = e.slice(i + 1);
         unique.add(`${local}@${domain}`);
     }
     return unique.size;
