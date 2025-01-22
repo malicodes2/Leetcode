c1854fa8b2eec9d3a23bf717c20da7dd9e7cae30
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-01-22 18:48:08 +0500

    Time: 0 ms (100.00%) | Memory: 49.5 MB (37.25%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,5 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    nums.sort((a, b) => a - b);
+    let i = 0, l = 0, r = nums.length - 1;
+    while (i <= r) {
+        if (nums[i] == 0) {
+            [nums[l], nums[i]] = [nums[i], nums[l]];
+            l++;
+        }
+        else if (nums[i] == 2) {
+            [nums[i], nums[r]] = [nums[r], nums[i]];
+            r--;
+            i--;
+        }
+        i++;
+    }
 };
