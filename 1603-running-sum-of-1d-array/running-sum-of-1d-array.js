24012573c5b15eff1b3a1cdcc5d660c7c0d0210e
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-02-18 15:37:41 +0500

    Time: 0 ms (100.00%) | Memory: 54.7 MB (5.89%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,8 +3,17 @@
  * @return {number[]}
  */
 var runningSum = function (nums) {
-    for (let i = 1; i < nums.length; i++) {
-        nums[i] = nums[i - 1] + nums[i];
-    }
-    return nums;
+    // for (let i = 1; i < nums.length; i++) {
+    //     nums[i] = nums[i - 1] + nums[i];
+    // }
+    // return nums;
+    return helper(nums, 1);
 };
+
+const helper = (nums, idx) => {
+    if (idx == nums.length) {
+        return nums;
+    }
+    nums[idx] += nums[idx - 1];
+    return helper(nums, idx + 1);
+}
