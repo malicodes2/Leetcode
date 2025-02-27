243d003c2d1a0d9ac8ae2a380c44e9d4cd94203d
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-02-27 18:40:58 +0500

    Time: 102 ms (44.24%) | Memory: 80.8 MB (5.22%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(arr) {
+    if (arr.length == 1) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2);
+    let left = sortArray(arr.slice(0, mid));
+    let right = sortArray(arr.slice(mid, arr.length));
+    return merge(left, right);
+};
+
+const merge = (first = [], second = []) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    while (i < first.length && j < second.length) {
+        if (first[i] < second[j]) {
+            result[k] = first[i];
+            i++;
+        } else {
+            result[k] = second[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < first.length) {
+        result[k] = first[i];
+        i++;
+        k++;
+    }
+
+    while (j < second.length) {
+        result[k] = second[j];
+        j++;
+        k++;
+    }
+    
+    return result;
+}
