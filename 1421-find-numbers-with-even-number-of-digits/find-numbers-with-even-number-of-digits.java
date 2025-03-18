fedb49bd19eb215b99d06b2fa20061b8875a17ef
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-03-18 20:02:44 +0500

    Time: 1 ms (98.42%) | Memory: 43.3 MB (30.89%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -15,11 +15,6 @@ class Solution {
     }
 
     int digits(int num) {
-        int count = 0;
-        while (num > 0) {
-            count++;
-            num = num / 10;
-        }
-        return count;
+        return (int) (Math.log10(num) + 1);
     }
 }
