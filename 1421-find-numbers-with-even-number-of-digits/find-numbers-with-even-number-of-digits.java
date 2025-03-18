000efcad65c2da69f254c24df45a87a1948b8995
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-03-18 19:51:10 +0500

    Time: 1 ms (98.42%) | Memory: 42.5 MB (94.91%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -2,19 +2,19 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for (int num : nums) {
-            if (even(num)){
+            if (even(num)) {
                 count++;
             }
         }
         return count;
     }
 
-    static boolean even(int num) {
+    boolean even(int num) {
         int numOfDigits = digits(num);
         return numOfDigits % 2 == 0;
     }
 
-    static int digits(int num) {
+    int digits(int num) {
         int count = 0;
         while (num > 0) {
             count++;
