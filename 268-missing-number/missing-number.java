4c7e8259044288178f2e81a9f075de5f892e9369
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-04-29 17:41:54 +0500

    Time: 1 ms (35.13%) | Memory: 45.7 MB (21.13%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (nums[idx] != idx) {
+                return idx;
+            }
+        }
+
+        return nums.length;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
