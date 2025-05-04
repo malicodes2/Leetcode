b1e374bc2dae3d9631acac180b8dc7edb7d6d706
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-05-04 20:47:50 +0500

    Time: 1 ms (99.99%) | Memory: 57.6 MB (33.98%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int idx = 0; idx < nums.length; idx++) {
+            if (nums[idx] != idx + 1) {
+                return idx + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
