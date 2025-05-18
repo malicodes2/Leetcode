257bf7825412eefebc8581aab938bbb17e3a4e4a
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-05-19 03:02:00 +0500

    Time: 0 ms (100.00%) | Memory: 40.4 MB (97.31%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -3,10 +3,8 @@ class Solution {
         int count = 0;
 
         while (n > 0) {
-            if ((n & 1) == 1) {
-                count++;
-            }
-            n = n >> 1;
+            n = n & (n - 1);
+            count++;
         }
         return count;
     }
