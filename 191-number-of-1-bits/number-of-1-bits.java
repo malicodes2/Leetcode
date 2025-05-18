9439a4e8e9c2ce0d0f8a3920287cba4014019fce
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-05-19 03:17:35 +0500

    Time: 0 ms (100.00%) | Memory: 41 MB (30.24%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,11 +1,12 @@
 class Solution {
     public int hammingWeight(int n) {
-        // Brian Kernighan’s Algorithm
         int count = 0;
 
         while (n > 0) {
-            n = n & (n - 1);
-            count++;
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >> 1;
         }
         return count;
     }
