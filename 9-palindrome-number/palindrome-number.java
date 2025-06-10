92ebd99c369559e82e5c070c51c0fe66ede4e6a0
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-06-10 13:31:56 +0500

    Time: 6 ms (32.98%) | Memory: 44.1 MB (62.55%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str = String.valueOf(x);
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        if (x < 0) {
+            return false;
+        } else {
+            return x == rev(x);
         }
-        return true;
+    }
+
+    static int rev(int n) {
+        int digits = (int) (Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    static int helper(int n, int digits) {
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int) (Math.pow(10, digits - 1)) + helper(n / 10, digits - 1);
     }
 }
