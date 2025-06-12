524702b9aace86122dac16397f49b1ea6c09b089
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-06-12 13:40:36 +0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (11.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,63 +1,30 @@
 class Solution {
     int search(int[] arr, int target) {
-        int pivot = findPivot(arr);
-
-        //if you can't find the pivot it means the array is not rotated.
-        if (pivot == -1) {
-            // just apply normal binary search
-            return binarySearch(arr, target, 0, arr.length - 1);
-        }
-
-        //if pivot is found, you have found 2 ascending sorted arrays
-        // Case 1
-        if (arr[pivot] == target) {
-            return pivot;
-        }
-
-        if (target >= arr[0]) {
-            return binarySearch(arr, target, 0, pivot - 1);
-        }
-        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+        return helper(arr, target, 0, arr.length - 1);
     }
 
-    int findPivot(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            // Case 1
-            if (mid < end && arr[mid] > arr[mid + 1]) {
-                return mid;
-            }
-            // Case 2
-            if (mid > start && arr[mid] < arr[mid - 1]) {
-                return mid - 1;
-            }
-            // Case 3
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            }
-            // Case 4
-            else {
-                start = mid + 1;
-            }
+    int helper(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
         }
-        return -1;
-    }
 
-    int binarySearch(int[] arr, int target, int start, int end) {
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        }
 
-            if (target > arr[mid]) {
-                start = mid + 1;
-            } else if (target < arr[mid]) {
-                end = mid - 1;
+        if (arr[s] <= arr[m]) {
+            if (target >= arr[s] && target <= arr[m]) {
+                return helper(arr, target, s, m - 1);
             } else {
-                return mid;
+                return helper(arr, target, m + 1, e);
             }
         }
-        return -1;
+
+        if (target >= arr[m] && target <= arr[e]) {
+            return helper(arr, target, m + 1, e);
+        } else {
+            return helper(arr, target, s, m - 1);
+        }
     }
 }
