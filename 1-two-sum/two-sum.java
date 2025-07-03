4ca4c047c68dfe83902ccd0323ba8afdc295bd6d
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-07-03 12:27:04 +0500

    Time: 2 ms (98.97%) | Memory: 45.2 MB (30.86%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int diff = target - nums[i];
+            int diff = target - nums[i]; // 9 - 2 = 7, 9 -7 = 2
 
             if (map.containsKey(diff)) {
                 return new int[] { map.get(diff), i };
@@ -11,6 +11,6 @@ class Solution {
 
             map.put(nums[i], i);
         }
-        return new int[] { -1, -1 };
+        return null;
     }
 }
