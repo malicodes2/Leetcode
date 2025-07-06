089902617e7c75c7b0b575be6e94c9dc85d78902
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-07-07 00:31:48 +0500

    Time: 3 ms (72.84%) | Memory: 43.5 MB (58.40%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        s = s.toLowerCase();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+                sb.append(c);
+            }
+        }
 
         int left = 0;
-        int right = s.length() - 1;
+        int right = sb.length() - 1;
 
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+        while (left <= right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
                 return false;
             }
             left++;
