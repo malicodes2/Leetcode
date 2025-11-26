4926f4a8f65e4e94443fd4e621a3c169c519926f
Muhammad Ali <111374423+malicodes2@users.noreply.github.com>
2025-11-26 18:33:10 +0500

    Time: 7 ms (13.47%) | Memory: 46.9 MB (7.90%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -16,52 +16,68 @@ class Solution {
             board.add(row.toString());
         }
 
-        solve(board, 0);
+        int startRow = 0;
+        HashSet<Integer> cols = new HashSet<Integer>();
+        HashSet<Integer> diags = new HashSet<Integer>();
+        HashSet<Integer> antiDiags = new HashSet<Integer>();
+        solve(board, startRow, cols, diags, antiDiags);
+
         return result;
     }
 
-    private boolean isValid(List<String> board, int row, int col) {
-        // Upward
-        for (int i = row; i >= 0; i--) {
-            if (board.get(i).charAt(col) == 'Q') {
-                return false;
-            }
-        }
+    // private boolean isValid(List<String> board, int row, int col) {
+    //     // Upward
+    //     for (int i = row; i >= 0; i--) {
+    //         if (board.get(i).charAt(col) == 'Q') {
+    //             return false;
+    //         }
+    //     }
 
-        // Left Diagonal Upward
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board.get(i).charAt(j) == 'Q') {
-                return false;
-            }
-        }
+    //     // Left Diagonal Upward
+    //     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    //         if (board.get(i).charAt(j) == 'Q') {
+    //             return false;
+    //         }
+    //     }
 
-        // Right Diagonal Upward
-        for (int i = row, j = col; i >= 0 && j < board.size(); i--, j++) {
-            if (board.get(i).charAt(j) == 'Q') {
-                return false;
-            }
-        }
+    //     // Right Diagonal Upward
+    //     for (int i = row, j = col; i >= 0 && j < board.size(); i--, j++) {
+    //         if (board.get(i).charAt(j) == 'Q') {
+    //             return false;
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    private void solve(List<String> board, int row) {
+    private void solve(List<String> board, int row, HashSet<Integer> cols, HashSet<Integer> diags, HashSet<Integer> antiDiags) {
         if (row == board.size()) {
             result.add(new ArrayList<>(board));
             return;
         }
 
-        for (int i = 0; i < board.size(); i++) {
-            if (isValid(board, row, i)) {
-                StringBuilder newRow = new StringBuilder(board.get(row));
-                newRow.setCharAt(i, 'Q');
-                board.set(row, newRow.toString());
+        for (int col = 0; col < board.size(); col++) {
+            int diag = row + col;
+            int antiDiag = row - col;
 
-                solve(board, row + 1);
-
-                newRow.setCharAt(i, '.');
-                board.set(row, newRow.toString());
+            if (cols.contains(col) || diags.contains(diag) || antiDiags.contains(antiDiag)) {
+                continue;
             }
+
+            cols.add(col);
+            diags.add(diag);
+            antiDiags.add(antiDiag);
+            StringBuilder newRow = new StringBuilder(board.get(row));
+            newRow.setCharAt(col, 'Q');
+            board.set(row, newRow.toString());
+
+            solve(board, row + 1, cols, diags, antiDiags);
+
+            cols.remove(col);
+            diags.remove(diag);
+            antiDiags.remove(antiDiag);
+            newRow.setCharAt(col, '.');
+            board.set(row, newRow.toString());
         }
     }
 }
